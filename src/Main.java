db5e7eddc351b1605eb4323ddc693f5a93737469
shadeisroot <dschristensen@proton.me>
2023-10-05 10:37:43 +0200

    this is a chracter

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void character(){
+        System.out.println("this is a character");
+    }
 }
